f087f80bd998f64bd10a8d37afa57ee50052d98f
Colin Chartier <colin.n.chartier@gmail.com>
2015-12-04 17:35:32 -0500

    Add tool bar

--- app/src/main/java/com/colinchartier/overture/app/activities/MainActivity.java
+++ app/src/main/java/com/colinchartier/overture/app/activities/MainActivity.java
@@ -2,12 +2,26 @@ package com.colinchartier.overture.app.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.colinchartier.overture.app.R;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.app_bar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        assert (getSupportActionBar() != null); //silly compiler, we're setting this already.
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //Don't show title on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white);
     }
 }
